15cb947da661989da63f4d0747a422f94d301e80
baris_aksay <barish.aksay@gmail.com>
2021-10-27 21:02:48 +0300

    growUp function added

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -5,3 +5,25 @@ describe('constructor', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
   });
+
+  describe('returns name property', () => {
+      it('should return the name of pet', ()=>{
+        const pet = new Pet ("fido");
+        expect(pet.name).toEqual("fido")
+      })
+
+
+  })
+
+
+  describe ('increment the age of pet by 1', () =>{
+      it('age is increased by 1', ()=>{
+        const pet = new Pet()
+        pet.growUp();
+        expect(pet.age).toBe(1);
+        
+        pet.age=999;
+        pet.growUp()
+        expect(pet.age).toBe(1000)
+      })
+  })
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,11 @@
-function Pet() {};
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+};
+
+Pet.prototype.growUp = function(){
+    return this.age++;
+}
+
 
 module.exports = Pet;
